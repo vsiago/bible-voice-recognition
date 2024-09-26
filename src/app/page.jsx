740c16f152ce023d811f5263be218f897d402ff3
vsiago <vsiago22@gmail.com>
2024-09-25 21:59:24 -0300

    teste2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,12 @@
-"use client"
-import { useState } from 'react';
-
 export default function Home() {
-  const [transcript, setTranscript] = useState('');
-  const [bibleText, setBibleText] = useState([]);
-  const [result, setResult] = useState('');
+
 
   
 
   return (
     <div style={{ padding: '20px' }}>
       <h1>Reconhecimento de Voz</h1>
-      <h2>Transcrição:</h2>
-      <p>{transcript}</p>
-      <h2>Resultado da Busca:</h2>
-      <p>{result}</p>
+
     </div>
   );
 }
